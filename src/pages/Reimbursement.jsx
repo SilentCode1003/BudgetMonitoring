44e83f632862387c21455a983baafadecf8eaef8
Rhaz05 <laurenrodrigueza9@gmail.com>
2023-07-10 14:40:56 +0800

    Added DropdownInput and added location lists display

--- src/pages/Reimbursement.jsx
+++ src/pages/Reimbursement.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import { Container, Card, Row, Col, Form, Button } from 'react-bootstrap';
+import DropdownInput from '../components/Dropdown-input';
 import Dropdown from '../components/Dropdown';
 import ReimburseTable from '../components/ReimburseTable';
 import { validateNumberInput } from '../components/RequestFunctions';
@@ -18,26 +19,26 @@ const Reimbursement = () => {
   const [locationDropdownValue, setLocationDropdownValue] = useState('');
   const [originDropdownValue, setOriginDropdownValue] = useState('');
   const [destinationDropdownValue, setDestinationDropdownValue] = useState('');
-  const [modeTransactionDropdownValue, setModeTransactionDropdownValue] = useState('');
+  const [modeTransportationDropdownValue, setModeTransportationDropdownValue] = useState('');
   const [reimburse, setReimburse] = useState([]);
 
   const locationDropdown = ['Pacita', 'Sta. Rosa', 'San Pedro', 'Calamba', 'Manila'];
   const originDropdown = ['Manila', 'Cavite', 'Batangas', 'Quezon'];
-  const destinationDropdown = ['Lucena', 'Buenavista', 'Gumaca', 'Sta. Rosa'];
-  const modeTransactionDropdown = ['Bus', 'Jeep', 'Van'];
+  const destinationDropdown = ['Lucena', 'Buenavista', 'Gumaca', 'Sta. Rosa', 'Pacita', 'Galleria', 'Caloocan', 'Lopez','Sta. Rosa', 'Pacita', 'Galleria', 'Caloocan', 'Lopez', ];
+  const modeTransportationDropdown = ['Bus', 'Jeep', 'Van'];
 
   const handleAddReimbursementClick = () => {
     handleAddReimbursement(
         locationDropdownValue,
         originDropdownValue,
         destinationDropdownValue,
-        modeTransactionDropdownValue,
+        modeTransportationDropdownValue,
         reimburse,
         setReimburse,
         setLocationDropdownValue,
         setOriginDropdownValue,
         setDestinationDropdownValue,
-        setModeTransactionDropdownValue,
+        setModeTransportationDropdownValue,
     );
   };
 
@@ -67,7 +68,7 @@ const Reimbursement = () => {
   return (
     <>
         <Row>
-          <Col  md={5} className="mt-4">
+          <Col  md={6} className="mt-4">
             <div className="dynamic-title-card">
               <Row>
                 <Col className='mt-2 mb-2' >
@@ -75,7 +76,7 @@ const Reimbursement = () => {
                 </Col>
               </Row>
             </div>
-            <Card>
+            <Card className='dynamic-card'>
               <Card.Body>
                 <Dropdown
                   options={locationDropdown}
@@ -89,17 +90,17 @@ const Reimbursement = () => {
                   value={originDropdownValue}
                   setValue={setOriginDropdownValue}
                 />
-                <Dropdown
+                <DropdownInput 
                   options={destinationDropdown}
-                  defaultOption="-- Select Destination --"
+                  defaultOption="-- Select/Input Destination --"
                   value={destinationDropdownValue}
                   setValue={setDestinationDropdownValue}
                 />
                 <Dropdown
-                  options={modeTransactionDropdown}
-                  defaultOption="-- Select Mode of Transaction --"
-                  value={modeTransactionDropdownValue}
-                  setValue={setModeTransactionDropdownValue}
+                  options={modeTransportationDropdown}
+                  defaultOption="-- Select Mode of Transportation --"
+                  value={modeTransportationDropdownValue}
+                  setValue={setModeTransportationDropdownValue}
                 />
                 <Form className="justify-content-center mt-2">
                   <Form.Group>
@@ -117,13 +118,26 @@ const Reimbursement = () => {
               </Card.Body>
             </Card>
           </Col>
-          <ReimburseTable
+          <Col md={6} className='mt-4'>
+            <div className="dynamic-title-card">
+              <Row>
+                <Col className='mt-2 mb-2' >
+                  <Card.Title>Location Lists</Card.Title>
+                </Col>
+              </Row>
+            </div>
+            <Card className='dynamic-card location-lists'>
+
+            </Card>
+          </Col>
+
+
+        </Row>
+        <ReimburseTable
             reimburse={reimburse}
             handleRemoveReimburse={handleRemoveReimburseClick}
             handleClearReimburse={handleClearReimburseClick}
-        />
-
-        </Row>
+          />
         <div className='reimbursement-table'>
           <DynamicTable title={"Reimbursement Table"} header={tableHeader} data={tableData} renderButtons={renderButtons}/>
         </div>
