f89c7e4caf3cec7d24863f2752f7004f14bfceda
SilentCode1003 <j0s3ph0r3nc10@gmail.com>
2023-12-16 09:18:22 +0800

    update header

--- src/pages/Request.jsx
+++ src/pages/Request.jsx
@@ -1,19 +1,24 @@
-import React, { useState, useEffect, useContext } from 'react';
-import Header from '../components/Header';
-import { Container, Card, Row, Col, Form, Button } from 'react-bootstrap';
-import Dropdown from '../components/Dropdown';
-import { useNavigate } from 'react-router-dom';
-import RequestTable from '../components/RequestTable';
-import { handleAddRequest, handleRemoveRequest, handleClearRequests, validateNumberInput } from '../components/RequestFunctions';
-import DynamicTable from '../components/DynamicTable';
-import { useGetConcern } from '../API/request/getConcern';
-import { useGetClientName } from '../API/request/getStoreName';
-import { useGetIssue } from '../API/request/getIssue';
-import { usePostRequest } from '../API/submit/postRequestBy';
-import ReimburseBtn from '../components/ReimburseBtn';
-import { UserContext } from '../components/userContext';
-import { usePostCancelRequest } from '../API/submit/postCancelRequest';
-import Swal from 'sweetalert2';
+import React, { useState, useEffect, useContext } from "react";
+import Header from "../components/header";
+import { Container, Card, Row, Col, Form, Button } from "react-bootstrap";
+import Dropdown from "../components/Dropdown";
+import { useNavigate } from "react-router-dom";
+import RequestTable from "../components/RequestTable";
+import {
+  handleAddRequest,
+  handleRemoveRequest,
+  handleClearRequests,
+  validateNumberInput,
+} from "../components/RequestFunctions";
+import DynamicTable from "../components/DynamicTable";
+import { useGetConcern } from "../API/request/getConcern";
+import { useGetClientName } from "../API/request/getStoreName";
+import { useGetIssue } from "../API/request/getIssue";
+import { usePostRequest } from "../API/submit/postRequestBy";
+import ReimburseBtn from "../components/ReimburseBtn";
+import { UserContext } from "../components/userContext";
+import { usePostCancelRequest } from "../API/submit/postCancelRequest";
+import Swal from "sweetalert2";
 
 const Request = () => {
   const { userData, updateRequestId } = useContext(UserContext);
@@ -29,96 +34,90 @@ const Request = () => {
   const navigate = useNavigate();
 
   //console.log(responseData.map((item) => item.status))
-  
+
   //console.log(btnFilter)
-  console.log(responseData)
+  console.log(responseData);
 
   useEffect(() => {
     const handlePostRequest = async () => {
       const requestData = {
-        requestby: employee
+        requestby: employee,
       };
       await postRequest.mutateAsync(requestData);
     };
-  
+
     if (employee) {
       handlePostRequest();
     }
   }, [employee]);
 
-  const renderButtons = (status, requestId) => {  
+  const renderButtons = (status, requestId) => {
     const handleReimburse = () => {
       console.log(`Reimburse Request ${requestId}.`);
       updateRequestId(requestId);
-      navigate('/Reimbursement', { state: { reload: true } });
+      navigate("/Reimbursement", { state: { reload: true } });
     };
 
     const handleCancel = async () => {
       console.log(`Cancel Request ${requestId}.`);
       const handlePostCancel = async () => {
         const cancelRequestId = {
-          requestid: requestId
+          requestid: requestId,
         };
         try {
           const response = await cancelRequest.mutateAsync({
-            requestId
+            requestId,
           });
-          console.log(response.msg)
-          if (response.msg === 'success') {
+          console.log(response.msg);
+          if (response.msg === "success") {
             await cancelRequest.mutateAsync(cancelRequestId);
             Swal.fire({
-              title: 'Success',
-              text: 'Request Cancelled Successful',
-              icon: 'success',
-              confirmButtonText: 'OK',
+              title: "Success",
+              text: "Request Cancelled Successful",
+              icon: "success",
+              confirmButtonText: "OK",
             }).then(() => {
               window.location.reload();
             });
           } else {
             Swal.fire({
-              title: 'Error',
-              text: 'Cancel Request Failed',
-              icon: 'error',
-              confirmButtonText: 'OK',
+              title: "Error",
+              text: "Cancel Request Failed",
+              icon: "error",
+              confirmButtonText: "OK",
             });
           }
         } catch (error) {
           Swal.fire({
-            title: 'Error',
-            text: 'Cancel Request Failed',
-            icon: 'error',
-            confirmButtonText: 'OK',
+            title: "Error",
+            text: "Cancel Request Failed",
+            icon: "error",
+            confirmButtonText: "OK",
           });
         }
       };
       handlePostCancel();
     };
-  
+
     switch (status) {
-      case 'APPROVED':
+      case "APPROVED":
         return (
           <Button variant="outline-danger w-100" onClick={handleReimburse}>
             Reimburse
           </Button>
         );
-      case 'REQUEST BUDGET':
+      case "REQUEST BUDGET":
         return (
           <Button variant="outline-danger w-100" onClick={handleCancel}>
             Cancel
           </Button>
         );
-      case 'CHECKING':
-        return (
-          <i>- - -</i>
-        );
-      case 'REIMBURSED':
-        return(
-          <i>- - -</i>
-        )
-      case 'DONE':
-        return(
-          <i>- - -</i>
-        )
+      case "CHECKING":
+        return <i>- - -</i>;
+      case "REIMBURSED":
+        return <i>- - -</i>;
+      case "DONE":
+        return <i>- - -</i>;
       default:
         return null;
     }
@@ -127,7 +126,7 @@ const Request = () => {
   const formattedResponseData = responseData.map((item) => {
     const actionButton = renderButtons(item.Status, item.RequestID);
     const details = JSON.parse(item.details);
-    let formattedDetails = '';
+    let formattedDetails = "";
     if (details.length <= 2) {
       formattedDetails = details.map((detail, index) => {
         const { ticketid, storename, concern, issue } = detail;
@@ -141,55 +140,64 @@ const Request = () => {
       formattedDetails = details.map((detail, index) => {
         const { ticketid, storename, concern, issue } = detail;
         return (
-          <div key={index}> 
+          <div key={index}>
             {index + 1}. {ticketid}, {storename}, {concern}, {issue}
           </div>
         );
       });
     }
     return {
-      'Request ID': item.requestid,
-      'Request By': item.requestby,
-      'Request Date': item.requestdate,
+      "Request ID": item.requestid,
+      "Request By": item.requestby,
+      "Request Date": item.requestdate,
       Budget: item.budget,
       Details: formattedDetails,
       Status: item.status,
     };
-  });  
-  
+  });
+
   //console.log(formattedResponseData);
   useEffect(() => {
     validateNumberInput();
   }, []);
 
-  const tableHeader = ['Request ID', 'Request By', 'Request Date', 'Budget', 'Details', 'Status'];
+  const tableHeader = [
+    "Request ID",
+    "Request By",
+    "Request Date",
+    "Budget",
+    "Details",
+    "Status",
+  ];
 
-  const [storeDropdownValue, setStoreDropdownValue] = useState('');
-  const [issueDropdownValue, setIssueDropdownValue] = useState('');
-  const [concernDropdownValue, setConcernDropdownValue] = useState('');
+  const [storeDropdownValue, setStoreDropdownValue] = useState("");
+  const [issueDropdownValue, setIssueDropdownValue] = useState("");
+  const [concernDropdownValue, setConcernDropdownValue] = useState("");
   const [filteredIssues, setFilteredIssues] = useState([]);
   const [filteredIssueNames, setFilteredIssueNames] = useState([]);
   const [requests, setRequests] = useState([]);
-  const [budget, setBudget] = useState('');
-  const [ticketId, setTicketId] = useState('');
+  const [budget, setBudget] = useState("");
+  const [ticketId, setTicketId] = useState("");
 
   useEffect(() => {
     if (concernDropdownValue && issues.length > 0) {
-      const filteredIssues = issues.filter((issue) => issue.concernname === concernDropdownValue);
+      const filteredIssues = issues.filter(
+        (issue) => issue.concernname === concernDropdownValue
+      );
       const issueNames = filteredIssues.map((issue) => issue.issuename);
       setFilteredIssues(filteredIssues);
       setFilteredIssueNames(issueNames);
-      setIssueDropdownValue('');
+      setIssueDropdownValue("");
     } else {
       setFilteredIssues([]);
       setFilteredIssueNames([]);
-      setIssueDropdownValue('');
+      setIssueDropdownValue("");
     }
   }, [concernDropdownValue, issues]);
 
   const handleConcernChange = (value) => {
     setConcernDropdownValue(value);
-    setIssueDropdownValue('');
+    setIssueDropdownValue("");
   };
 
   const handleAddRequestClick = () => {
@@ -207,7 +215,7 @@ const Request = () => {
       setBudget
     );
 
-    setTicketId('');
+    setTicketId("");
   };
 
   const handleRemoveRequestClick = (index) => {
@@ -224,12 +232,12 @@ const Request = () => {
         <Col className="mt-4">
           <div className="dynamic-title-card">
             <Row>
-              <Col className='mt-2 mb-2' >
+              <Col className="mt-2 mb-2">
                 <Card.Title>Budget</Card.Title>
               </Col>
             </Row>
           </div>
-          <Card className='dynamic-card'>
+          <Card className="dynamic-card">
             <Card.Body>
               <Form className="justify-content-center">
                 <Form.Group>
@@ -248,12 +256,12 @@ const Request = () => {
 
           <div className="dynamic-title-card mt-2">
             <Row>
-              <Col className='mt-2 mb-2' >
+              <Col className="mt-2 mb-2">
                 <Card.Title>Store</Card.Title>
               </Col>
             </Row>
           </div>
-          <Card className='dynamic-card'>
+          <Card className="dynamic-card">
             <Card.Body>
               <Form.Control
                 className="number-validator"
@@ -270,7 +278,10 @@ const Request = () => {
                   setValue={setStoreDropdownValue}
                 />
               ) : (
-                <button className="btn-primary w-100 dropdown-display mt-2" disabled>
+                <button
+                  className="btn-primary w-100 dropdown-display mt-2"
+                  disabled
+                >
                   No Store Name Available
                 </button>
               )}
@@ -282,7 +293,10 @@ const Request = () => {
                   setValue={handleConcernChange}
                 />
               ) : (
-                <button className="btn-primary w-100 dropdown-display mt-2" disabled>
+                <button
+                  className="btn-primary w-100 dropdown-display mt-2"
+                  disabled
+                >
                   No Concern Available
                 </button>
               )}
@@ -290,20 +304,27 @@ const Request = () => {
                 <Dropdown
                   options={filteredIssueNames}
                   defaultOption={
-                    issueDropdownValue && filteredIssueNames.includes(issueDropdownValue)
+                    issueDropdownValue &&
+                    filteredIssueNames.includes(issueDropdownValue)
                       ? issueDropdownValue
-                      : '--- Select Issue ---'
+                      : "--- Select Issue ---"
                   }
                   value={issueDropdownValue}
                   setValue={setIssueDropdownValue}
                 />
               ) : (
-                <button className="btn-primary w-100 dropdown-display mt-2" disabled>
+                <button
+                  className="btn-primary w-100 dropdown-display mt-2"
+                  disabled
+                >
                   No Issue Available
                 </button>
               )}
               <div className="button-container d-flex justify-content-end mt-2">
-                <Button variant="outline-danger" onClick={handleAddRequestClick}>
+                <Button
+                  variant="outline-danger"
+                  onClick={handleAddRequestClick}
+                >
                   Add Request
                 </Button>
               </div>
@@ -320,11 +341,11 @@ const Request = () => {
         />
       </Row>
       <div className="reimbursement-table">
-        <DynamicTable 
-          title="Reimbursement Table" 
-          header={tableHeader} 
-          data={formattedResponseData} 
-          renderButtons={renderButtons} 
+        <DynamicTable
+          title="Reimbursement Table"
+          header={tableHeader}
+          data={formattedResponseData}
+          renderButtons={renderButtons}
         />
       </div>
     </>
